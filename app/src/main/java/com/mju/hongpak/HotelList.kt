7aa8d4d0a246b095be57c44f6852de2fa3be45ca
Maythawat Mahawan <moking55@hotmail.com>
2024-03-10 06:03:51 +0700

    feat: add hotel list page

---
+++ app/src/main/java/com/mju/hongpak/HotelList.kt
@@ -0,0 +1,11 @@
+package com.mju.hongpak
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class HotelList : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_hotel_list)
+    }
+}
